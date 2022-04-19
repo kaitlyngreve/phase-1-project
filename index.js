8e732f7b17a0628da6f94f8d59bb4ff1ace5e77f
Jovana Agobian <jovana.agobian@gmail.com>
2022-04-19 16:17:21 -0400

    mouseover, renderActivity function

--- index.js
+++ index.js
@@ -4,20 +4,31 @@ const baseUrl = "https://www.boredapi.com/api/activity"
 function fetchActivities() {
     fetch(baseUrl)
     .then(resp => resp.json())
-    .then(activityObj => renderAllActivities(activityObj))
+    .then(activityObj => renderActivity(activityObj))
 }
 
 
 
 //callback functions
 
-function renderAllActivities(activityObj) {
+function renderActivity(activityObj) {
+    console.log(activityObj)
+    // console.log(activityObj.activity)
     const actContainer = document.querySelector('#activity-results')
-    activityObj.forEach(actKey => {
-    const actTitle = document.createElement('p')
-    actTitle.textContent = actKey.activity
-    console.log(actTitle)
-    })
+    const activityElement = document.createElement('h4')
+    activityElement.textContent = activityObj.activity
+    // console.log(activityElement)
+    actContainer.append(activityElement)
 }
 
 fetchActivities()
+
+// mouse over event
+const btn = document.getElementById('generator-button');
+btn.addEventListener('mouseover', function(e) {
+    e.target.style.backgroundColor = 'blue';
+})
+
+btn.addEventListener('mouseout', function(e) {
+    e.target.style.backgroundColor = 'tomato';
+})
