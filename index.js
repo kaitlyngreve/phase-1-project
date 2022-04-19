c46de1cfd316bea1966a7248f4314e4fd29edc59
Jovana Agobian <jovana.agobian@gmail.com>
2022-04-19 10:02:11 -0400

    Fetch requestz

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let animal = 'dog'
+const baseUrl = "https://www.boredapi.com/api/activity"
+
+function fetchActivities() {
+    fetch(baseUrl)
+    .then(resp => resp.json())
+    .then(activityObj => console.log(activityObj))
+}
+
+fetchActivities()
