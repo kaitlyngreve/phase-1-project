329f8452cfaa2357bd4dc19c6d809aa3e5032f64
Jovana Agobian <jovana.agobian@gmail.com>
2022-04-19 10:52:43 -0400

    first callback function

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const baseUrl = "https://www.boredapi.com/api/activity"
 
+//fetch request
 function fetchActivities() {
     fetch(baseUrl)
     .then(resp => resp.json())
-    .then(activityObj => console.log(activityObj))
+    .then(activityObj => renderAllActivities(activityObj))
+}
+
+
+
+//callback functions
+
+function renderAllActivities(activityObj) {
+    const actContainer = document.querySelector('#activity-results')
+    activityObj.forEach(actKey => {
+    const actTitle = document.createElement('p')
+    actTitle.textContent = actKey.activity
+    console.log(actTitle)
+    })
 }
 
 fetchActivities()
