e7fc07cef08af064307cb7274f8df91affc5b82a
Kaitlyn Greve <kgreve14@gmail.com>
2022-04-21 16:01:15 -0400

    fixed API bug

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function handleSubmitForm(e) {
         } 
     }).value
     console.log(filterActivity)
-    fetch(`http://www.boredapi.com/api/activity?type=${filterActivity}`)
+    fetch(`https://www.boredapi.com/api/activity?type=${filterActivity}`)
     .then(resp => resp.json())
     .then(activityObj => renderActivity(activityObj))
 
